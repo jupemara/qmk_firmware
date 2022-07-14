4b472012b41f201f345194cd546befaf3e6e82db
jupemara <jumpei.arashi@arashike.com>
2022-07-14 15:33:14 +0900

    media play / stop is now space + adjust

--- keyboards/planck/keymaps/jupemara/keymap.c
+++ keyboards/planck/keymaps/jupemara/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RALT,
-    KC_MPLY, KC_LEFT, KC_RGHT, KC_LGUI, RAISE,   KC_SPC,  KC_BSPC,  LOWER,  KC_TAB,  KC_DOWN, KC_UP,   KC_RGHT
+    KC_MPLY, KC_LEFT, KC_DOWN, KC_LGUI, RAISE,   KC_SPC,  KC_BSPC,  LOWER,  KC_TAB,  KC_UP,   KC_RGHT, KC_RGUI
 ),
 
 /* Lower
@@ -99,9 +99,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_A] = LAYOUT_planck_grid(
     RESET,   XXXXXXX, XXXXXXX,  KC_MUTE, KC_VOLD, KC_VOLU, KC_WH_U, KC_WH_D, KC_PGDN, KC_PGUP, XXXXXXX, KC_EJCT,
-    XXXXXXX, XXXXXXX,M_SS_MEM, M_SS_DSK, KC_MPLY, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX,M_SS_MEM, M_SS_DSK, XXXXXXX, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, KC_MPRV, KC_MNXT, KC_BTN1, KC_BTN2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    XXXXXXX, XXXXXXX, XXXXXXX,  _______, XXXXXXX, KC_MPLY, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_TOG
 )
 
 };
@@ -122,22 +122,11 @@ const key_override_t **key_overrides = (const key_override_t *[]){
     NULL
 };
 
-#ifdef AUDIO_ENABLE
-  float plover_song[][2]     = SONG(PLOVER_SOUND);
-  float plover_gb_song[][2]  = SONG(PLOVER_GOODBYE_SOUND);
-#endif
-
-bool muse_mode = false;
-uint8_t last_muse_note = 0;
-uint16_t muse_counter = 0;
-uint8_t muse_offset = 70;
-uint16_t muse_tempo = 50;
-
 bool encoder_update_user(uint8_t index, bool clockwise) {
   if (clockwise) {
     tap_code(KC_VOLU);
   } else {
     tap_code(KC_VOLD);
   }
-  return true;
+  return false;
 }
