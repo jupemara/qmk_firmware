df22af91845f65af33c6ed28053f88183c752199
jupemara <jumpei.arashi@arashike.com>
2022-09-26 20:19:46 +0900

    now right bksp is with shift

--- keyboards/elephant42/keymaps/jupemara/keymap.c
+++ keyboards/elephant42/keymaps/jupemara/keymap.c
@@ -17,8 +17,9 @@
 
 #define SS_MEM LGUI(LSFT(LCTL(KC_4)))
 #define SS_DSK LGUI(LSFT(KC_4))
-#define KC_LSPC LT(1, KC_SPC)
-#define KC_RSPC LT(2, KC_SPC)
+#define MT_LSFT MT(MOD_LSFT, KC_SPC)
+#define MT_RSFT MT(MOD_RSFT, KC_BSPC)
+#define MT_RALT MT(MOD_RALT, KC_ENT)
 #define LOWER LT(1, KC_LANG2)
 #define RAISE LT(2, KC_LANG1)
 
@@ -32,15 +33,15 @@ enum layer_names {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_QWERTY] = LAYOUT( \
+  [_QWERTY] = LAYOUT(
   //,-------+-------+-------+-------+-------+-------.                                    ,-------+-------+-------+-------+-------+-------.
      KC_ESC , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                                      KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_BSPC,
   //|-------+-------+-------+-------+-------+-------|                                    |-------+-------+-------+-------+-------+-------|
-     KC_LCTL, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                                      KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_RALT,
+     KC_LCTL, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                                      KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,MT_RALT,
   //`-------+-------+-------+-------+-------+-------|                                    |-------+-------+-------+-------+-------+-------'
               KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                                      KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,
   //        `-------+-------+-------+-------+-------+-------+-------.    ,-------+-------+-------+-------+-------+-------+-------'
-                                     KC_LGUI,KC_LSFT,LOWER  ,KC_SPC ,     KC_BSPC,RAISE  ,KC_ENT ,KC_TAB
+                                     KC_LGUI,KC_LSFT,LOWER  ,KC_SPC,      MT_RSFT,RAISE  ,KC_ENT, KC_TAB
   //                                `-------+-------+-------+-------'    `-------+-------+-------+-------'
   ),
 
@@ -48,25 +49,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-------+-------+-------+-------+-------+-------.                                    ,-------+-------+-------+-------+-------+-------.
      _______, KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                                      KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_DEL,
   //|-------+-------+-------+-------+-------+-------|                                    |-------+-------+-------+-------+-------+-------|
-     _______,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,                                     KC_LEFT,KC_DOWN,KC_UP, KC_RIGHT,KC_QUOT,KC_ENT,
+     _______,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,                                     KC_LEFT,KC_DOWN,KC_UP, KC_RIGHT,KC_QUOT,_______,
   //`-------+-------+-------+-------+-------+-------|                                    |-------+-------+-------+-------+-------+-------|
              KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,                                     KC_MINS,KC_PLUS,KC_LABK,KC_RABK,KC_BSLS,
   //        `-------+-------+-------+-------+-------+-------+-------.    ,-------+-------+-------+-------+-------+-------+-------'
-                                     _______,_______,_______,_______,     KC_DEL, MO(3),  _______,_______
+                                     _______,_______,_______,_______,     MT_RSFT, MO(3),  _______,_______
   //                                `-------+-------+-------+-------'    `-------+-------+-------+-------'
   ),
 
   [_RAISE] = LAYOUT(
   //,-------+-------+-------+-------+-------+-------.                                    ,-------+-------+-------+-------+-------+-------.
-     _______, KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                                      KC_F6 , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
+     _______, KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                                      KC_F6 , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_DEL,
   //|-------+-------+-------+-------+-------+-------|                                    |-------+-------+-------+-------+-------+-------|
-     _______,KC_QUOT,KC_DQUO,KC_GRV, KC_MINS,KC_PLUS,                                     KC_LBRC,KC_RBRC,KC_LCBR,KC_RCBR,KC_PIPE,KC_F12 ,
+     _______,KC_QUOT,KC_DQUO,KC_GRV, KC_MINS,KC_PLUS,                                     KC_LBRC,KC_RBRC,KC_LCBR,KC_RCBR,KC_PIPE,_______,
   //`-------+-------+-------+-------+-------+-------|                                    |-------+-------+-------+-------+-------+-------'
              _______,_______,KC_TILD,KC_UNDS,KC_EQL,                                     KC_LEFT,KC_DOWN,KC_UP,  KC_RIGHT,KC_BSLS,
   //        `-------+-------+-------+-------+-------+-------+-------.    ,-------+-------+-------+-------+-------+-------+-------'
-                                     _______,_______,MO(3),  KC_LSFT,     KC_DEL ,_______,_______,_______
+                                     _______,_______,MO(3),  _______,     MT_RSFT,_______,_______,_______
   //                                `-------+-------+-------+-------'    `-------+-------+-------+-------'
-  ),
+
 
   [_ADJUST] = LAYOUT(
   //,-------+-------+-------+-------+-------+-------.                                    ,-------+-------+-------+-------+-------+-------.
@@ -76,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //`-------+-------+-------+-------+-------+-------|                                    |-------+-------+-------+-------+-------+-------'
              RGB_TOG,RGB_VAI,RGB_MOD,RGB_VAD,RGB_HUI,                                     KC_BTN1,KC_BTN2,KC_HOME,KC_END, XXXXXXX,
   //        `-------+-------+-------+-------+-------+-------+-------.    ,-------+-------+-------+-------+-------+-------+-------'
-                                     KC_MPRV,KC_MNXT,_______,KC_MPLY,     KC_DEL ,_______,KC_ENT ,KC_BSPC
+                                     KC_MPRV,_______,KC_MNXT,KC_MPLY,     KC_DEL ,_______,XXXXXXX,XXXXXXX
   //                                `-------+-------+-------+-------'    `-------+-------+-------+-------'
   )
 };
@@ -94,7 +95,7 @@ const key_override_t gui_grv = ko_make_basic(
   KC_ESC,
   G(KC_GRV)
 );
-
+  
 const key_override_t **key_overrides = (const key_override_t *[]){
     &ctl_tab,
     &gui_grv,
@@ -117,7 +118,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 bool oled_task_user(void) {
     if (is_keyboard_master()) {
-        static char layer_names[NUMBER_OF_LAYERS][10] = {"Default", "Lower", "Raise"};
+        static char layer_names[NUMBER_OF_LAYERS][10] = {"Default", "Lower", "Raise", "Adjust"};
         static char l1[] = "                \x94\x95\x96\x97";
         static char l2[] = "                \xB4\xB5\xB6\xB7";
         static char r1[] = "                \x98\x99\x9A\x9B";
